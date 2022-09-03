e86d6792e56b8d49c7711391b3c16ad93dfdc75d
AMartins87 <aneta.martins@outlook.com>
2022-09-03 19:58:08 +0000

    Added category filtering

--- games/views.py
+++ games/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Game
+from .models import Game, Category
 
 
 def all_games(request):
     """ A view to show all games, including sorting and search queries """
 
     games = Game.objects.all()
-    query = None
+    query = ""
+    categories = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            games = games.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -19,12 +25,12 @@ def all_games(request):
                 return redirect(reverse('games'))
 
         queries = Q(name__icontains=query) | Q(description__icontains=query)
-
         games = games.filter(queries)
 
     context = {
         'games': games,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'games/games.html', context)
