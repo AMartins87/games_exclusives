6d4db4733be843831ba68ce4685202f594248d5d
AMartins87 <aneta.martins@outlook.com>
2022-09-03 19:34:42 +0000

    added game details functionality

--- games/urls.py
+++ games/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_games, name='games')
+    path('', views.all_games, name='games'),
+    path('<game_id>', views.game_detail, name='game_detail'),
 ]
--- games/views.py
+++ games/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Game
 
 
 def all_games(request):
-    """ A view to show all products, including sorting and search queries """
+    """ A view to show all games, including sorting and search queries """
 
     games = Game.objects.all()
 
@@ -12,3 +12,15 @@ def all_games(request):
     }
 
     return render(request, 'games/games.html', context)
+
+
+def game_detail(request, game_id):
+    """ A view to show individual game details """
+
+    game = get_object_or_404(Game, pk=game_id)
+
+    context = {
+        'game': game,
+    }
+
+    return render(request, 'games/game_detail.html', context)
