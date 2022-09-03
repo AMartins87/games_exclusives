e5cd25d7f40402b0d31538d8739aaf5461005623
AMartins87 <aneta.martins@outlook.com>
2022-09-03 19:44:32 +0000

    Added search functionality

--- games/views.py
+++ games/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Game
 
 
@@ -6,9 +8,23 @@ def all_games(request):
     """ A view to show all games, including sorting and search queries """
 
     games = Game.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter"
+                               "any search criteria!")
+                return redirect(reverse('games'))
+
+        queries = Q(name__icontains=query) | Q(description__icontains=query)
+
+        games = games.filter(queries)
 
     context = {
         'games': games,
+        'search_term': query,
     }
 
     return render(request, 'games/games.html', context)
