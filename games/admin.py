f116c1b1566bb2f5e301aa7ae56547c5c0732ae1
AMartins87 <aneta.martins@outlook.com>
2022-09-03 16:49:20 +0000

    customized admin

--- games/admin.py
+++ games/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
 from .models import Game, Category
 
-# Register your models here.
+
+class GameAdmin(admin.ModelAdmin):
+    """Tells admin which fields to display """
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'image',
+    )
+
+    """Sorts the products by SKU using the ordering attribute """
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
 admin.site.register(Game)
 admin.site.register(Category)
