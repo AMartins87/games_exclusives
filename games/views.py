333a850a988df08105606785caa913168dfa4182
AMartins87 <aneta.martins@outlook.com>
2022-09-03 16:59:51 +0000

    added games views and template

--- games/views.py
+++ games/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Game
 
-# Create your views here.
+
+def all_games(request):
+    """ A view to show all products, including sorting and search queries """
+
+    games = Game.objects.all()
+
+    context = {
+        'games': games,
+    }
+
+    return render(request, 'games/games.html', context)
