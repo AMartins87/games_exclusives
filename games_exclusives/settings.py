84305dc2e9b453b02b6b262004e759fcc9c70606
AMartins87 <aneta.martins@outlook.com>
2022-09-10 16:23:52 +0000

    Added basket context and delivery logic

--- games_exclusives/settings.py
+++ games_exclusives/settings.py
@@ -73,6 +73,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'basket.contexts.basket_contents',
             ],
         },
     },
@@ -154,6 +155,10 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
+
 
 
 # Default primary key field type
