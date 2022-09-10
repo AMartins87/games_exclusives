17148275e7b4456ea78ffbf8c42acc0f02e8f95f
AMartins87 <aneta.martins@outlook.com>
2022-09-10 16:43:56 +0000

    Updated context processor

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,11 +1,25 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from games.models import Game
+
 
 def basket_contents(request):
 
     basket_items = []
     total = 0
     game_count = 0
+    basket = request.session.get('basket', {})
+
+    for item_id, quantity in basket.items():
+        game = get_object_or_404(Game, pk=item_id)
+        total += quantity * game.price
+        game_count += quantity
+        basket_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'game': game,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
