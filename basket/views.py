02221ff82c833a38ef3858c39a663716cfdc2b39
AMartins87 <aneta.martins@outlook.com>
2022-09-11 08:31:25 +0000

    added adjust_basket,remove_from_basket views and calc subtotal template in basket_tools

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import (
+     render, redirect, reverse, HttpResponse, get_object_or_404
+)
+from django.contrib import messages
+from games.models import Game
 
 
 def view_basket(request):
@@ -8,7 +12,7 @@ def view_basket(request):
 
 
 def add_to_basket(request, item_id):
-    """ Add a quantity of the specified product to the shopping basket """
+    """ Adds quantity of chosen game to the shopping basket """
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
@@ -21,3 +25,38 @@ def add_to_basket(request, item_id):
 
     request.session['basket'] = basket
     return redirect(redirect_url)
+
+
+def adjust_basket(request, item_id):
+    """ Adjusts the quantity of chosen game to selected amount """
+
+    game = get_object_or_404(Game, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    basket = request.session.get('basket', {})
+
+    if quantity > 0:
+        basket[item_id] = quantity
+        messages.success(
+            request, f'Updated {game.name} quantity to {basket[item_id]}'
+            )
+    else:
+        basket.pop(item_id)
+        messages.success(request, f'You have succcessfully removed {game.name} from your basket!')
+
+    request.session['basket'] = basket
+    return redirect(reverse('view_basket'))
+
+
+def remove_from_basket(request, item_id):
+    """Remove the item from the shopping basket"""
+    try:
+        game = get_object_or_404(Game, pk=item_id)
+        basket = request.session.get('basket', {})
+        basket.pop(item_id)
+        messages.success(request, f'Removed {game.name} from your basket')
+
+        request.session['basket'] = basket
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, f'Error removing game: {e}')
+        return HttpResponse(status=500)
