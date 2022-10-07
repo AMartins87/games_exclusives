17cc7bf7eb17c8011f60de92d4ebfa908a04dce3
AMartins87 <aneta.martins@outlook.com>
2022-10-07 11:14:11 +0000

    Deployment part 2

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn games_exclusives.wsgi:application
--- games_exclusives/settings.py
+++ games_exclusives/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-nh%v$5xo*u8++84)fs44f@!h=$!(c=xdn09=_buno8$+6d^a#6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['games-exclusives.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -125,12 +125,17 @@ WSGI_APPLICATION = 'games_exclusives.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
