0f1363d37898d380969c7dc360047a0d770319c7
AMartins87 <aneta.martins@outlook.com>
2022-10-07 13:44:45 +0000

    Install and set cloudinary

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://clnbqcbsggbmmv:c68071e8d72fe0e9b2322ae1685865ea4ef43a5efdfff3f7282f8f682716efd6@ec2-34-247-72-29.eu-west-1.compute.amazonaws.com:5432/ddt2naetj6ohr7"
+os.environ["SECRET_KEY"] = "ds#bo%4o4aeoj-kp@4!x$v+v$da*7b&1-5*jxl07ygql8t&f9s"
+os.environ["CLOUDINARY_URL"] = "cloudinary://571977319717261:qCCALCeTf9qWp7KuLtgF0Xb7SW4@games-exclusives"
--- games_exclusives/settings.py
+++ games_exclusives/settings.py
@@ -10,9 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-import os
 from pathlib import Path
+import os
 import dj_database_url
+
+if os.path.isfile('env.py'):
+    import env
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -39,7 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
@@ -179,10 +184,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_STORAGE = ('cloudinary_storage.storage.
+                       'StaticHashedCloudinaryStorage')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Stripe
 FREE_DELIVERY_THRESHOLD = 50
