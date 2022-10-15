1640922da2effaa442abfc1428955075fe04906b
AMartins87 <aneta.martins@outlook.com>
2022-10-15 15:19:40 +0000

    Change header styling and footer margin

--- games/forms.py
+++ games/forms.py
@@ -11,7 +11,9 @@ class GameForm(forms.ModelForm):
         model = Game
         fields = '__all__'
 
-    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
+    image = forms.ImageField(label='Image',
+                             required=False,
+                             widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
